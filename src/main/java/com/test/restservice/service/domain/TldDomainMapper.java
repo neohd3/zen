e100d4ae8cf26d5cc8814cc5feab70cfb136303b
Valerio <dilerarc@gmail.com>
2020-01-28 12:28:35 +0300

    fix mapper

--- src/main/java/com/test/restservice/service/domain/TldDomainMapper.java
+++ src/main/java/com/test/restservice/service/domain/TldDomainMapper.java
@@ -1,11 +1,14 @@
 package com.test.restservice.service.domain;
 
+import com.test.restservice.dao.model.Domain;
 import com.test.restservice.dao.model.Tld;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+@Slf4j
 @Component
 public class TldDomainMapper {
 
@@ -13,7 +16,9 @@ public class TldDomainMapper {
 
         if (tld == null) return null;
 
-        String domainName = tld.getDomains().isEmpty() ? null : tld.getDomains().get(0).getName();
+        log.info(tld + "");
+        String domainName = tld.getDomains().isEmpty() ? null :
+                tld.getDomains().stream().filter(e -> e.getName().equals(domain)).findFirst().map(Domain::getName).orElse(null);
 
         return new TldDomainDTO(
                 domain + "." + tld.getName(),
